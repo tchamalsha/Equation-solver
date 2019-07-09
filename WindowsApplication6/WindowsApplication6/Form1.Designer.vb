772cc494d3f4a46494f1e29db155c5a85d1260ea
Sadisha Nimsara <42792030+nsadisha@users.noreply.github.com>
2019-07-09 21:46:30 +0530

    Add files via upload

---
+++ WindowsApplication6/WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbly = New System.Windows.Forms.Label()
+        Me.lblx = New System.Windows.Forms.Label()
+        Me.txtr = New System.Windows.Forms.TextBox()
+        Me.txtq = New System.Windows.Forms.TextBox()
+        Me.txtp = New System.Windows.Forms.TextBox()
+        Me.txtc = New System.Windows.Forms.TextBox()
+        Me.txta = New System.Windows.Forms.TextBox()
+        Me.txtb = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbly
+        '
+        Me.lbly.AutoSize = True
+        Me.lbly.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbly.Location = New System.Drawing.Point(369, 266)
+        Me.lbly.Name = "lbly"
+        Me.lbly.Size = New System.Drawing.Size(68, 33)
+        Me.lbly.TabIndex = 51
+        Me.lbly.Text = "Y = "
+        '
+        'lblx
+        '
+        Me.lblx.AutoSize = True
+        Me.lblx.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblx.Location = New System.Drawing.Point(182, 266)
+        Me.lblx.Name = "lblx"
+        Me.lblx.Size = New System.Drawing.Size(67, 33)
+        Me.lblx.TabIndex = 50
+        Me.lblx.Text = "X = "
+        '
+        'txtr
+        '
+        Me.txtr.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtr.Location = New System.Drawing.Point(499, 154)
+        Me.txtr.Name = "txtr"
+        Me.txtr.Size = New System.Drawing.Size(55, 35)
+        Me.txtr.TabIndex = 49
+        '
+        'txtq
+        '
+        Me.txtq.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtq.Location = New System.Drawing.Point(388, 154)
+        Me.txtq.Name = "txtq"
+        Me.txtq.Size = New System.Drawing.Size(55, 35)
+        Me.txtq.TabIndex = 48
+        '
+        'txtp
+        '
+        Me.txtp.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtp.Location = New System.Drawing.Point(272, 154)
+        Me.txtp.Name = "txtp"
+        Me.txtp.Size = New System.Drawing.Size(55, 35)
+        Me.txtp.TabIndex = 47
+        '
+        'txtc
+        '
+        Me.txtc.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtc.Location = New System.Drawing.Point(499, 46)
+        Me.txtc.Name = "txtc"
+        Me.txtc.Size = New System.Drawing.Size(55, 35)
+        Me.txtc.TabIndex = 46
+        '
+        'txta
+        '
+        Me.txta.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txta.Location = New System.Drawing.Point(272, 46)
+        Me.txta.Name = "txta"
+        Me.txta.Size = New System.Drawing.Size(55, 35)
+        Me.txta.TabIndex = 45
+        '
+        'txtb
+        '
+        Me.txtb.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtb.Location = New System.Drawing.Point(388, 46)
+        Me.txtb.Name = "txtb"
+        Me.txtb.Size = New System.Drawing.Size(55, 35)
+        Me.txtb.TabIndex = 44
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(147, 362)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(102, 35)
+        Me.Button2.TabIndex = 43
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(515, 126)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(19, 25)
+        Me.Label8.TabIndex = 42
+        Me.Label8.Text = "r"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(404, 126)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(24, 25)
+        Me.Label7.TabIndex = 41
+        Me.Label7.Text = "q"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(286, 126)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(24, 25)
+        Me.Label6.TabIndex = 40
+        Me.Label6.Text = "p"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(515, 18)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 25)
+        Me.Label5.TabIndex = 39
+        Me.Label5.Text = "c"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(404, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(24, 25)
+        Me.Label4.TabIndex = 38
+        Me.Label4.Text = "b"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(375, 362)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 35)
+        Me.Button1.TabIndex = 37
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(286, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 25)
+        Me.Label2.TabIndex = 36
+        Me.Label2.Text = "a"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(30, 154)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 33)
+        Me.Label1.TabIndex = 35
+        Me.Label1.Text = "pX + qY + r = 0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(29, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(198, 33)
+        Me.Label3.TabIndex = 34
+        Me.Label3.Text = "aX + bY + c = 0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 415)
+        Me.Controls.Add(Me.lbly)
+        Me.Controls.Add(Me.lblx)
+        Me.Controls.Add(Me.txtr)
+        Me.Controls.Add(Me.txtq)
+        Me.Controls.Add(Me.txtp)
+        Me.Controls.Add(Me.txtc)
+        Me.Controls.Add(Me.txta)
+        Me.Controls.Add(Me.txtb)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbly As System.Windows.Forms.Label
+    Friend WithEvents lblx As System.Windows.Forms.Label
+    Friend WithEvents txtr As System.Windows.Forms.TextBox
+    Friend WithEvents txtq As System.Windows.Forms.TextBox
+    Friend WithEvents txtp As System.Windows.Forms.TextBox
+    Friend WithEvents txtc As System.Windows.Forms.TextBox
+    Friend WithEvents txta As System.Windows.Forms.TextBox
+    Friend WithEvents txtb As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication6/WindowsApplication6/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, c, p, q, r, x, y As String
+        a = Val(txta.Text)
+        b = Val(txtb.Text)
+        c = Val(txtc.Text)
+        p = Val(txtp.Text)
+        q = Val(txtq.Text)
+        r = Val(txtr.Text)
+
+        x = ((r * b) + (q * c)) / ((b * p) - (q * a))
+        y = ((p * c) - (a * r)) / ((q * a) - (p * b))
+
+        lblx.Text = "X = " + x
+        lbly.Text = "Y = " + y
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        txta.Text = ""
+        txtb.Text = ""
+        txtc.Text = ""
+        txtp.Text = ""
+        txtq.Text = ""
+        txtr.Text = ""
+    End Sub
+End Class
